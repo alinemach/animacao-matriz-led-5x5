2bdfc51a110b42bdcbd1ffaabab5a49714d910c9
Eng-Sauan <sauan.s1920@ufob.edu.br>
2025-01-28 20:44:51 -0300

    Implemento de código referente a animação 3

--- animacao-matriz-led-5x5.c
+++ animacao-matriz-led-5x5.c
@@ -7,9 +7,11 @@
 #include "hardware/clocks.h"
 #include "hardware/adc.h"
 #include "pico/bootrom.h"
+#include "hardware/pwm.h"
 
 //arquivo .pio
 #include "animacao-matriz-led-5x5.pio.h"
+#include "ws2818b.pio.h"
 
 // UART defines
 // By default the stdout UART is `uart0`, so we will use the second one
@@ -134,6 +136,380 @@ char teclado_scan() {
     return 0;  // Nenhuma tecla pressionada
 }
 
+//CÓDIGO REFERENTE A ANIMAÇÃO 3
+// Biblioteca gerada pelo arquivo .pio durante compilação.
+
+
+#define BUZZER 21 //PINO 21 DEFINIDO COMO BUZZER.
+#define DO 261.63 //Nota Dó
+#define DOh 277.18 //Dó#
+#define RE 293.66 //Nota Ré
+#define REh 311.13//Ré#
+#define MI 329.63 //Nota Mi
+#define FA 349.23 //Nota Fá
+#define FAh 369.99//Fá#0,,,,,,,
+#define SOL 392 //Nota Sol
+#define SOLh 415.30 //Sol#
+#define LA 440 //Nota Lá
+#define LAh 466.16 //Lá#
+#define SI 493.88 //Nota Si
+#define SIh 493.88 //Sí#
+
+// Definição do número de LEDs e pino.
+#define LED_COUNT 25
+#define LED_PIN 7
+
+int matriz1[5][5][3] = {
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {250, 249, 241}, {255, 242, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}}
+};
+
+int matriz2[5][5][3] = {
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{33, 54, 255}, {0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {250, 249, 241}, {255, 242, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}}
+};
+
+int matriz3[5][5][3] = {
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{255, 242, 0}, {33, 54, 255}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {250, 249, 241}, {255, 242, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}}
+};
+
+int matriz4[5][5][3] = {
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}},
+    {{255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {250, 249, 241}, {255, 242, 0}},
+    {{0, 0, 0}, {255, 242, 0}, {255, 242, 0}, {255, 242, 0}, {0, 0, 0}}
+};
+
+int matriz5[5][5][3] = {
+    {{33, 54, 255}, {0, 0, 0}, {33, 54, 255}, {0, 0, 0}, {33, 54, 255}},
+    {{33, 54, 255}, {33, 54, 255}, {33, 54, 255}, {33, 54, 255}, {33, 54, 255}},
+    {{33, 54, 255}, {33, 54, 255}, {33, 54, 255}, {33, 54, 255}, {33, 54, 255}},
+    {{33, 54, 255}, {0, 0, 0}, {33, 54, 255}, {0, 0, 0}, {33, 54, 255}},
+    {{0, 0, 0}, {33, 54, 255}, {33, 54, 255}, {33, 54, 255}, {0, 0, 0}}
+};
+
+int matriz6[5][5][3] = {
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}},
+    {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}}
+};
+
+int linha;
+
+// Definição de pixel GRB
+struct pixel_t {
+  uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+    };
+    typedef struct pixel_t pixel_t;
+    typedef pixel_t npLEED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+
+    // Declaração do buffer de pixels que formam a matriz.
+    npLEED_t leeds[LED_COUNT];
+
+    // Variáveis para uso da máquina PIO.
+    PIO np_pio;
+    uint sm;
+
+/**
+ * Inicializa a máquina PIO para controle da matriz de LEDs.
+ */
+void npInit1(uint pin) {
+
+  // Cria programa PIO.
+  uint offset = pio_add_program(pio0, &ws2818b_program);
+  np_pio = pio0;
+
+  // Toma posse de uma máquina PIO.
+  sm = pio_claim_unused_sm(np_pio, false);
+  if (sm < 0) {
+    np_pio = pio1;
+    sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+  }
+
+  // Inicia programa na máquina PIO obtida.
+  ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+  // Limpa buffer de pixels.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    leds[i].R = 0;
+    leds[i].G = 0;
+    leds[i].B = 0;
+  }
+}
+
+/**
+ * Atribui uma cor RGB a um LED.
+ */
+void npSetLED1(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
+  leds[index].R = r;
+  leds[index].G = g;
+  leds[index].B = b;
+}
+
+/**
+ * Limpa o buffer de pixels.
+ */
+void npClear1() {
+  for (uint i = 0; i < LED_COUNT; ++i)
+    npSetLED1(i, 0, 0, 0);
+}
+
+/**
+ * Escreve os dados do buffer nos LEDs.
+ */
+void npWrite1() {
+  // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+  for (uint i = 0; i < LED_COUNT; ++i) {
+    pio_sm_put_blocking(np_pio, sm, leds[i].G);
+    pio_sm_put_blocking(np_pio, sm, leds[i].R);
+    pio_sm_put_blocking(np_pio, sm, leds[i].B);
+  }
+  sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
+}
+
+// Função para converter a posição do matriz para uma posição do vetor.
+int getIndex(int x, int y) {
+    // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+    // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+    if (y % 2 == 0) {
+        return y * 5 + x; // Linha par (esquerda para direita).
+    } else {
+        return y * 5 + (4 - x); // Linha ímpar (direita para esquerda).
+    }
+}
+
+void BUZZER_CONFIG (uint gpio, float frequencia, float duty_cycle){
+    gpio_set_function(gpio, GPIO_FUNC_PWM); //DEFINE O PINO COMO SAÍDA PWM
+    uint slice_num = pwm_gpio_to_slice_num(gpio); //OBTEM O NUMERO DA SLICE ASSOCIADA AO PINO
+    uint channel = pwm_gpio_to_channel(gpio); //OBETEM O NÚMERO DO CANAL
+
+    float clk_div = 8.0f;  // DIVISOR DE CLOCK
+    uint32_t wrap = (uint32_t)((125e6 / (frequencia * clk_div)) - 1);
+   
+    // CONFIGURAÇÃO PWM
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, clk_div);
+    pwm_config_set_wrap(&config, wrap);
+    pwm_init(slice_num, &config, true);
+    
+    // CONFIGURAÇÃO DUTY_CICLE
+     uint32_t level = (uint32_t)(wrap * duty_cycle);
+    pwm_set_chan_level(slice_num, channel, level);
+    pwm_set_enabled(slice_num, true);
+
+}
+
+void CONTROLE_BUZZER(){
+
+    BUZZER_CONFIG(BUZZER, DO, 0.5f); //CONFIGURA FREQUENCIA E DUTY_CYCLE
+        sleep_ms(500);
+        
+        
+        uint slice_num = pwm_gpio_to_slice_num(BUZZER);
+        uint channel = pwm_gpio_to_channel(BUZZER);
+        pwm_set_chan_level(slice_num, channel, 0);  //DESLIGAR BUZZER
+
+}
+// Função para desligar o buzzer
+void DESLIGAR_BUZZER() {
+    uint slice_num = pwm_gpio_to_slice_num(BUZZER);
+    uint channel = pwm_gpio_to_channel(BUZZER);
+    pwm_set_chan_level(slice_num, channel, 0);  // Desliga o buzzer
+    pwm_set_enabled(slice_num, false);
+}
+
+// Função para tocar várias notas
+void TOCAR_NOTAS() {
+    float notas[] = {SI, SI, FAh, REh, SI, FAh, REh,
+DO, DO, SOL, MI, DO, SOL, MI,
+SI, SI, FAh, DOh, SI, FAh, REh,
+RE, REh, MI, FA, SOL, SOLh, LA, LAh, SI}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(200);  // Intervalo de 200ms entre as notas
+        DESLIGAR_BUZZER();  // Desliga o buzzer após tocar a nota
+        
+    }
+}
+
+// Função para tocar várias notas
+void TOCAR_SOM() {
+    float notas[] = {600}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(50);  // Intervalo de 150ms entre as notas
+    float notas[] = {0}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(20);  // Intervalo de 150ms entre as notas        
+        DESLIGAR_BUZZER();  // Desliga o buzzer após tocar a nota
+    }
+    }
+}
+
+void TOCAR_SOM_FANTASMA() {
+    float notas[] = {MI}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(120);  // Intervalo de 150ms entre as notas
+    float notas[] = {0}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(10);  // Intervalo de 150ms entre as notas        
+        DESLIGAR_BUZZER();  // Desliga o buzzer após tocar a nota
+
+        float notas[] = {DO}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(120);  // Intervalo de 150ms entre as notas        
+        DESLIGAR_BUZZER();  // Desliga o buzzer após tocar a nota
+
+        float notas[] = {0}; // Array com as notas a serem tocadas
+    int num_notas = sizeof(notas) / sizeof(notas[0]);
+
+    for (int i = 0; i < num_notas; i++) {
+        BUZZER_CONFIG(BUZZER, notas[i], 0.5f); // Toca cada nota com duty cycle de 50%
+        sleep_ms(10);  // Intervalo de 150ms entre as notas        
+        DESLIGAR_BUZZER();  // Desliga o buzzer após tocar a nota
+    }
+    }
+    }
+    }
+}
+
+void FRAMEINICIAL(){
+      for (int y = 0; y < 5; y++) {
+        for (int x = 0; x < 5; x++) {
+          int index = getIndex(x, y);
+          npSetLED1(index, matriz1[y][x][0], matriz1[y][x][1], matriz1[y][x][2]);
+      }
+    }
+    npWrite1(); // Escreve os dados nos LEDs.
+    TOCAR_NOTAS();
+}
+
+
+void FRAME1(){
+      for (int y = 0; y < 5; y++) {
+        for (int x = 0; x < 5; x++) {
+          int index = getIndex(x, y);
+          npSetLED1(index, matriz1[y][x][0], matriz1[y][x][1], matriz1[y][x][2]);
+      }
+    }
+}
+
+void FRAME2(){
+      for (int y = 0; y < 5; y++) {
+        for (int x = 0; x < 5; x++) {
+          int index = getIndex(x, y);
+          npSetLED1(index, matriz2[y][x][0], matriz2[y][x][1], matriz2[y][x][2]);
+      }
+    }
+}
+
+void FRAME3(){
+    for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+        int index = getIndex(x, y);
+        npSetLED1(index, matriz3[y][x][0], matriz3[y][x][1], matriz3[y][x][2]);
+      }
+    }
+}
+
+void FRAME4(){
+      for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+        int index = getIndex(x, y);
+        npSetLED1(index, matriz4[y][x][0], matriz4[y][x][1], matriz4[y][x][2]);
+      }
+    }
+}
+
+void FRAME5(){
+      for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+        int index = getIndex(x, y);
+        npSetLED1(index, matriz5[y][x][0], matriz5[y][x][1], matriz5[y][x][2]);
+      }
+    }
+}
+
+void FRAME6(){
+      for (int y = 0; y < 5; y++) {
+      for (int x = 0; x < 5; x++) {
+        int index = getIndex(x, y);
+        npSetLED1(index, matriz6[y][x][0], matriz6[y][x][1], matriz6[y][x][2]);
+      }
+    }
+}
+
+void LEDS() {
+
+  // Inicializa entradas e saídas.
+  stdio_init_all();
+
+  // Inicializa matriz de LEDs NeoPixel.
+  npInit1(LED_PIN);
+  npClear1();
+
+    // Atualiza os LEDs com o padrão da matriz.
+    FRAMEINICIAL();
+    npWrite1(); // Escreve os dados nos LEDs.
+    sleep_ms(1500); // Delay de 1 segundo antes de repetir.
+   for (int i=0; i<5; i++){
+    TOCAR_SOM();
+    FRAME1();
+    npWrite1();
+    sleep_ms(100);
+    FRAME2();
+    npWrite1();
+    sleep_ms(100);
+    FRAME3();
+    npWrite1();
+    sleep_ms(100);
+    FRAME4();
+    npWrite1();
+    sleep_ms(100);
+   }
+  for (int v=0; v<5; v++){
+    TOCAR_SOM_FANTASMA();
+    FRAME5();
+    npWrite1();
+    sleep_ms(500);
+    FRAME6();
+    npWrite1();
+    sleep_ms(500);
+  }
+  }
+
+
+
+
 // Função para definir a intensidade das cores do LED
 uint32_t matrix_rgb(double b, double r, double g) {
     unsigned char R, G, B;
@@ -241,6 +617,10 @@ int main() {
                     break;
                 case '2':
                     animacao2();
+                    break;
+
+                case '3':
+                    LEDS();
                     break;
                 default:
                     // Caso nenhuma tecla seja pressionada, a matriz de LEDs é apagada
